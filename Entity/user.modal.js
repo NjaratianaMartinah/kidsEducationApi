3df3b5416d47802956177101149814df6dd16c70
Njaratiana <stagiaire.it@ravinala-airports.aero>
2022-05-14 11:21:07 +0200

    create  user entity

---
+++ Entity/user.modal.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    id:{
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+UserSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        //do not reveal passwordHash
+        delete returnedObject.password
+    }
+})
+
+ const User = mongoose.model("user", UserSchema);
+
+ module.exports = { User , UserSchema };
