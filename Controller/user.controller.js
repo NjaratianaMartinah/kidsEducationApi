11c3db2ab41fbf2c0d1784f647b51c1215f8558a
Njaratiana <stagiaire.it@ravinala-airports.aero>
2022-05-14 13:21:56 +0200

    create response helper, success and error function , add to user controller

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -1,15 +1,18 @@
 const userService = require("../Service/user.service");
+const response = require("../Helper/response.helper");
 
 const userController  = (url , router) => {
     console.log(url);
     router.post( `${url}/login`, (req, res) => {
-        console.log("login");
+        userService.login(req.body).then( profil => response.success(res, profil, 'Logged in successfully'))
+        .catch(error => response.error(res, "Login failed"));
     });
 
     router.post(`${url}/register`, (req, res) => {
         console.log("register");
         console.log(req.body);
-        userService.register(req.body).then( () => {res.body("OK")});
+        userService.register(req.body).then( () => { response.success(res, null, "registration finished")})
+        .catch(err => response.error(res, " An error occured during the registration"));
     })
 }
 
